a6477726a7d3da140c1d8ec5bc96eae696533969
Muhammad Thanveer Akula <muhammadthanveer111@gmail.com>
2026-08-31 21:58:23 +0530

    Add: climbing-stairs - submission-0

---
+++ Data Structures & Algorithms/climbing-stairs/submission-0.java
@@ -0,0 +1,27 @@
+class Solution {
+    public int climbStairs(int n) {
+        
+        int dp[] = new int[n+1];
+
+        Arrays.fill(dp,-1);
+
+        return find(n,dp);
+    }
+
+    static int find(int n,int dp[])
+    {
+        if(n == 0 || n == 1)
+        {
+            return 1;
+        }
+
+        if(dp[n] != -1)
+        {
+            return dp[n];
+        }
+
+        dp[n] = find(n-1,dp) + find(n-2,dp);
+
+        return dp[n];
+    }
+}
